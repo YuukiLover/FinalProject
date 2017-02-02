a84a83c50066129cc0b992ea43b5f71523b96418
samuel <samuelc.dele@gmail.com>
2017-02-02 09:33:44 +0100

    Init project

---
+++ src/main/java/com/sddr/rpg/FinalProject/App.java
@@ -0,0 +1,9 @@
+package com.sddr.rpg.FinalProject;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/com/sddr/rpg/FinalProject/AppTest.java
@@ -0,0 +1,12 @@
+package com.sddr.rpg.FinalProject;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
