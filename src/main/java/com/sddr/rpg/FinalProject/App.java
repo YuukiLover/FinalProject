c4b45970ccf2fa28a6f6f16b5c238f99c92e5a59
YuukiLover <samuelc.dele@gmail.com>
2017-02-02 12:44:09 +0100

    BDD
    Ajout couche DAO

--- src/main/java/com/sddr/rpg/FinalProject/App.java
+++ src/main/java/com/sddr/rpg/FinalProject/App.java
@@ -1,9 +1,24 @@
 package com.sddr.rpg.FinalProject;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import com.sddr.rpg.connection.ConnectionProvider;
+
 public class App {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+	public static void main(String[] args) {
+
+		
+		EntityManager em = ConnectionProvider.getConnection();
+		// get and open a new Transaction with database
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		
+
+		tx.commit();
+		ConnectionProvider.closeConnection();
+	}
 
 }
